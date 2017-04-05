cdfb57abf3b532129e0ff8799fca5164b804a19b
teej107 <teej107@gmail.com>
2017-04-04 20:39:05 -0600

    Basic login

--- Web Client/js/directive/navbar/navbar.js
+++ Web Client/js/directive/navbar/navbar.js
@@ -7,9 +7,13 @@ app.directive('navbar', function ()
         templateUrl: getDirectiveDirectory() + "/navbar/navbar.html",
         restrict: 'E',
         scope: {},
-        controller: function ($scope, $window)
+        controller: function ($scope, $window, authService)
         {
             $scope.loginTitle = "Login";
+            authService.getUserNotNull().then(function (success)
+            {
+               $scope.loginTitle = success.name.givenName;
+            });
             $scope.login = function ()
             {
                 $window.location.href = "/auth/google";
--- Web Client/js/service/authService.js
+++ Web Client/js/service/authService.js
@@ -1,11 +1,49 @@
 /**
  * Created by tanner on 4/4/17.
  */
-app.service('authService', function ()
+app.service('authService', function ($http, $q)
 {
-   var user = null;
-   this.getUser = function ()
-   {
-       return user;
-   }
+    var user = null;
+
+    function httpFetchUser(resolve, reject)
+    {
+        $http({
+            url: '/user',
+            method: 'GET'
+        }).then(function (success)
+        {
+            user = success.data;
+            resolve(user);
+        }, function (failure)
+        {
+            reject(failure);
+        })
+    }
+
+    this.fetchUser = function ()
+    {
+        return $q(function (resolve, reject)
+        {
+            httpFetchUser(resolve, reject);
+        });
+
+    };
+    this.getUser = function ()
+    {
+        return user;
+    };
+    this.getUserNotNull = function ()
+    {
+        return $q(function (resolve, reject)
+        {
+            if (user)
+            {
+                resolve(user);
+            }
+            else
+            {
+                httpFetchUser(resolve, reject);
+            }
+        });
+    }.bind(this);
 });
